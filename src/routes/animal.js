a10469f4c292edb7cfb572d95d1dabba62447578
Laura-Vanessa <jvanessarincon13@gmail.com>
2024-09-11 17:35:36 -0500

    actualizar
    actualizar

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
-//Nuevo animal
+//Nuevo animal
 router.post("/animals", (req, res) => {
 const animal = animalSchema(req.body);
 animal
@@ -9,4 +9,4 @@ animal
 .then((data) => res.json(data))
 .catch((error) => res.json({ message: error }));
 });
-module.exports = router;
+module.exports = router;
